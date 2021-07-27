75b5d4c626310cc6ac93ef2103b7bb97a412f770
aviv20-meet <aviv20@meet.mit.edu>
2021-07-27 18:15:37 +0300

    another attempt

--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
 import os
-from cachelib.file import FileSystemCache 
+from flask_caching import Cache
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.urandom(24) 
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+app.config.from_mapping(config)
 
 print(__name__)
 
-c = FileSystemCache('/localCache')
+c = Cache(app)
 
 from server import routes
